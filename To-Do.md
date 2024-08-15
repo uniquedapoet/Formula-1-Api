f7523a73862f9f6ae7a1ff55f10630ead4e23af6
uniquedapoet <eddiebenjamin86@gmail.com>
2024-08-14 21:44:47 -0600

    chore: Integrate driver endpoint with specific filtering

---
+++ To-Do.md
@@ -0,0 +1,14 @@
+# To-Do
+
+## Frontend
+
+- Integrate search's with each other
+- UI
+- Styling
+
+## Backend 
+
+- Integrate endpoints into each other
+- pictures
+- data duplication
+
--- backend/app/routes/results.py
+++ backend/app/routes/results.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
+from sqlalchemy import text as sqltext
 from app.database import SessionLocal
 from typing import List
 from app.models.db_models import Results as DBResult  # Import the SQLAlchemy model
@@ -23,9 +24,24 @@ def get_results(db: Session = Depends(get_db)):
     return [PydanticResults.from_orm(result) for result in results]
 
 
-@router.get("/results/{date}", response_model=PydanticResults)
-def get_result(date: str, db: Session = Depends(get_db)):
+@router.get("/results/{}", response_model=PydanticResults)
+def get_result_date(date: str, db: Session = Depends(get_db)):
     results = db.query(DBResult).filter(DBResult.date == date).all()
     if not results:
         raise HTTPException(status_code=404, detail="Result not found")
+    return [PydanticResults.from_orm(result) for result in results]
+
+
+@router.get("/results/years/{year}", response_model=List[PydanticResults])
+def get_result_year(year: int, db: Session = Depends(get_db)):
+    query = sqltext("""  
+        SELECT DISTINCT ON (circuitname) *
+        FROM results
+        WHERE year = :year
+        ORDER BY circuitname
+    """)   
+    results = db.execute(query, {"year": year}).fetchall()
+    if not results:
+        raise HTTPException(status_code=404, detail="Result not found")
+    
     return [PydanticResults.from_orm(result) for result in results]
