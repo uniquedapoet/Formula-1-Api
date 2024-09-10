e601192568a8d4d35454207ed20df6fad4357609
uniquedapoet <eddiebenjamin86@gmail.com>
2024-09-10 16:16:20 -0600

    chore: Update FastAPI configuration and add project description

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,12 +5,28 @@ from app.data_loader import load_circuits_data, load_drivers_data, load_results_
 from datetime import datetime, timedelta
 from contextlib import asynccontextmanager
 import uvicorn
-import sys, os
+import sys
+import os
 from . import last_load_time  # Import the variable from __init__.py
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-app = FastAPI()
+app = FastAPI(
+    title="Formula 1 API",
+    description="""
+    This RESTful API provides detailed data on the Formula 1 World Championship, including information on drivers, circuits, and race results. Built with FastAPI and powered by a PostgreSQL database, this project helps deepen my understanding of API architecture and backend development. The frontend, developed with React, allows users to search for and retrieve specific race information. Though still in development, I am continuously adding new features and improving the user experience.
+    """,
+    version="0.1",
+    docs_url="/docs",
+    redoc_url='/redoc',
+    contact={
+        'name': 'Eduardo Benjamin',
+        'email': 'eddiebenjamin86@gmail.com',
+        'url': 'https://eduardobenjamin.site'  
+    }
+)
+
+
 
 # Add CORS middleware
 app.add_middleware(
@@ -30,12 +46,13 @@ app.include_router(results.router)
 def read_root():
     return {"message": "Hello World"}
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     global last_load_time
     start_time = datetime.now()
     print("Starting up...", start_time.hour)
-   
+
     # Check if an hour has passed since the last load time
     if start_time - last_load_time >= timedelta(hours=1):
         load_circuits_data()
